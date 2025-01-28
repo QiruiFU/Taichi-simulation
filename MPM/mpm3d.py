9099b99ee2ae6fc80d1d32547d0aee9ba5a15744
fuqirui <846639569@qq.com>
2025-01-28 15:42:08 -0500

    finish mpm/snow. Need to inscrease resolution

--- MPM/mpm3d.py
+++ MPM/mpm3d.py
@@ -1,322 +1,153 @@
-import numpy as np
-
 import taichi as ti
 
-ti.init(arch=ti.gpu)
+ti.init(arch=ti.gpu)  # Try to run on GPU
 
-# dim, n_grid, steps, dt = 2, 128, 20, 2e-4
-# dim, n_grid, steps, dt = 2, 256, 32, 1e-4
-# dim, n_grid, steps, dt = 3, 32, 25, 4e-4
-dim, n_grid, steps, dt = 3, 64, 25, 2e-4
-# dim, n_grid, steps, dt = 3, 128, 5, 1e-4
-
-n_particles = n_grid**dim // 2 ** (dim - 1)
-
-print(n_particles)
-
-dx = 1 / n_grid
-
-p_rho = 1
-p_vol = (dx * 0.5) ** 2
+quality = 1  # Use a larger value for higher-res simulations
+n_particles, n_grid = 9000 * quality**2, 16 * quality
+dx, inv_dx = 1 / n_grid, float(n_grid)
+dt = 1e-4 / quality
+p_vol, p_rho = (dx * 0.5) ** 2, 1
 p_mass = p_vol * p_rho
-GRAVITY = [0, -9.8, 0]
-bound = 3
-E = 1000  # Young's modulus
-nu = 0.2  #  Poisson's ratio
+E, nu = 5e3, 0.2  # Young's modulus and Poisson's ratio
 mu_0, lambda_0 = E / (2 * (1 + nu)), E * nu / ((1 + nu) * (1 - 2 * nu))  # Lame parameters
 
-F_x = ti.Vector.field(dim, float, n_particles)
-F_v = ti.Vector.field(dim, float, n_particles)
-F_C = ti.Matrix.field(dim, dim, float, n_particles)
-F_dg = ti.Matrix.field(3, 3, dtype=float, shape=n_particles)  # deformation gradient
-F_Jp = ti.field(float, n_particles)
-
-F_colors = ti.Vector.field(4, float, n_particles)
-F_colors_random = ti.Vector.field(4, float, n_particles)
-F_materials = ti.field(int, n_particles)
-F_grid_v = ti.Vector.field(dim, float, (n_grid,) * dim)
-F_grid_m = ti.field(float, (n_grid,) * dim)
-F_used = ti.field(int, n_particles)
-
-neighbour = (3,) * dim
-
-WATER = 0
-JELLY = 1
-SNOW = 2
+x = ti.Vector.field(2, dtype=float, shape=n_particles)  # position
+v = ti.Vector.field(2, dtype=float, shape=n_particles)  # velocity
+C = ti.Matrix.field(2, 2, dtype=float, shape=n_particles)  # affine velocity field
+F = ti.Matrix.field(2, 2, dtype=float, shape=n_particles)  # deformation gradient
+material = ti.field(dtype=int, shape=n_particles)  # material id
+Jp = ti.field(dtype=float, shape=n_particles)  # plastic deformation
+grid_v = ti.Vector.field(2, dtype=float, shape=(n_grid, n_grid))  # grid node momentum/velocity
+grid_m = ti.field(dtype=float, shape=(n_grid, n_grid))  # grid node mass
+gravity = ti.Vector.field(2, dtype=float, shape=())
+attractor_strength = ti.field(dtype=float, shape=())
+attractor_pos = ti.Vector.field(2, dtype=float, shape=())
 
 
 @ti.kernel
-def substep(g_x: float, g_y: float, g_z: float):
-    for I in ti.grouped(F_grid_m):
-        F_grid_v[I] = ti.zero(F_grid_v[I])
-        F_grid_m[I] = 0
-    ti.loop_config(block_dim=n_grid)
-    for p in F_x:
-        if F_used[p] == 0:
-            continue
-        Xp = F_x[p] / dx
-        base = int(Xp - 0.5)
-        fx = Xp - base
+def substep():
+    for i, j in grid_m:
+        grid_v[i, j] = [0, 0]
+        grid_m[i, j] = 0
+    for p in x:  # Particle state update and scatter to grid (P2G)
+        base = (x[p] * inv_dx - 0.5).cast(int)
+        fx = x[p] * inv_dx - base.cast(float)
+        # Quadratic kernels  [http://mpm.graphics   Eqn. 123, with x=fx, fx-1,fx-2]
         w = [0.5 * (1.5 - fx) ** 2, 0.75 - (fx - 1) ** 2, 0.5 * (fx - 0.5) ** 2]
-
-        F_dg[p] = (ti.Matrix.identity(float, 3) + dt * F_C[p]) @ F_dg[p]  # deformation gradient update
+        # deformation gradient update
+        F[p] = (ti.Matrix.identity(float, 2) + dt * C[p]) @ F[p]
         # Hardening coefficient: snow gets harder when compressed
-        h = ti.exp(10 * (1.0 - F_Jp[p]))
-        if F_materials[p] == JELLY:  # jelly, make it softer
-            h = 0.3
+        # h = ti.max(0.1, ti.min(5, ti.exp(10 * (1.0 - Jp[p]))))
+        # h = ti.exp(10 * (1.0 - Jp[p]))
+        h = 1
         mu, la = mu_0 * h, lambda_0 * h
-        if F_materials[p] == WATER:  # liquid
-            mu = 0.0
-
-        U, sig, V = ti.svd(F_dg[p])
+        U, sig, V = ti.svd(F[p])
         J = 1.0
-        for d in ti.static(range(3)):
+        for d in ti.static(range(2)):
             new_sig = sig[d, d]
-            if F_materials[p] == SNOW:  # Snow
-                new_sig = ti.min(ti.max(sig[d, d], 1 - 2.5e-2), 1 + 4.5e-3)  # Plasticity
-            F_Jp[p] *= sig[d, d] / new_sig
+            if material[p] == 2:  # Snow
+                new_sig = min(max(sig[d, d], 1 - 2.5e-2 ), 1 + 4.5e-3 )  # Plasticity
+            Jp[p] *= sig[d, d] / new_sig
             sig[d, d] = new_sig
             J *= new_sig
-        if F_materials[p] == WATER:
-            # Reset deformation gradient to avoid numerical instability
-            new_F = ti.Matrix.identity(float, 3)
-            new_F[0, 0] = J
-            F_dg[p] = new_F
-        elif F_materials[p] == SNOW:
-            # Reconstruct elastic deformation gradient after plasticity
-            F_dg[p] = U @ sig @ V.transpose()
-        stress = 2 * mu * (F_dg[p] - U @ V.transpose()) @ F_dg[p].transpose() + ti.Matrix.identity(
-            float, 3
-        ) * la * J * (J - 1)
-        stress = (-dt * p_vol * 4) * stress / dx**2
-        affine = stress + p_mass * F_C[p]
-
-        for offset in ti.static(ti.grouped(ti.ndrange(*neighbour))):
-            dpos = (offset - fx) * dx
-            weight = 1.0
-            for i in ti.static(range(dim)):
-                weight *= w[offset[i]][i]
-            F_grid_v[base + offset] += weight * (p_mass * F_v[p] + affine @ dpos)
-            F_grid_m[base + offset] += weight * p_mass
-    for I in ti.grouped(F_grid_m):
-        if F_grid_m[I] > 0:
-            F_grid_v[I] /= F_grid_m[I]
-        F_grid_v[I] += dt * ti.Vector([g_x, g_y, g_z])
-        cond = (I < bound) & (F_grid_v[I] < 0) | (I > n_grid - bound) & (F_grid_v[I] > 0)
-        F_grid_v[I] = ti.select(cond, 0, F_grid_v[I])
-    ti.loop_config(block_dim=n_grid)
-    for p in F_x:
-        if F_used[p] == 0:
-            continue
-        Xp = F_x[p] / dx
-        base = int(Xp - 0.5)
-        fx = Xp - base
-        w = [0.5 * (1.5 - fx) ** 2, 0.75 - (fx - 1) ** 2, 0.5 * (fx - 0.5) ** 2]
-        new_v = ti.zero(F_v[p])
-        new_C = ti.zero(F_C[p])
-        for offset in ti.static(ti.grouped(ti.ndrange(*neighbour))):
-            dpos = (offset - fx) * dx
-            weight = 1.0
-            for i in ti.static(range(dim)):
-                weight *= w[offset[i]][i]
-            g_v = F_grid_v[base + offset]
-            new_v += weight * g_v
-            new_C += 4 * weight * g_v.outer_product(dpos) / dx**2
-        F_v[p] = new_v
-        F_x[p] += dt * F_v[p]
-        F_C[p] = new_C
-
-
-class CubeVolume:
-    def __init__(self, minimum, size, material):
-        self.minimum = minimum
-        self.size = size
-        self.volume = self.size.x * self.size.y * self.size.z
-        self.material = material
-
-
-@ti.kernel
-def init_cube_vol(
-    first_par: int,
-    last_par: int,
-    x_begin: float,
-    y_begin: float,
-    z_begin: float,
-    x_size: float,
-    y_size: float,
-    z_size: float,
-    material: int,
-):
-    for i in range(first_par, last_par):
-        F_x[i] = ti.Vector([ti.random() for i in range(dim)]) * ti.Vector([x_size, y_size, z_size]) + ti.Vector(
-            [x_begin, y_begin, z_begin]
+        # Reconstruct elastic deformation gradient after plasticity
+        F[p] = U @ sig @ V.transpose()
+        stress = 2 * mu * (F[p] - U @ V.transpose()) @ F[p].transpose() + ti.Matrix.identity(float, 2) * la * J * (
+            J - 1
         )
-        F_Jp[i] = 1
-        F_dg[i] = ti.Matrix([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
-        F_v[i] = ti.Vector([0.0, 0.0, 0.0])
-        F_materials[i] = material
-        F_colors_random[i] = ti.Vector([ti.random(), ti.random(), ti.random(), ti.random()])
-        F_used[i] = 1
+        stress = (-dt * p_vol * 4 * inv_dx * inv_dx) * stress
+        affine = stress + p_mass * C[p]
+        for i, j in ti.static(ti.ndrange(3, 3)):
+            # Loop over 3x3 grid node neighborhood
+            offset = ti.Vector([i, j])
+            dpos = (offset.cast(float) - fx) * dx
+            weight = w[i][0] * w[j][1]
+            grid_v[base + offset] += weight * (p_mass * v[p] + affine @ dpos)
+            grid_m[base + offset] += weight * p_mass
+    for i, j in grid_m:
+        if grid_m[i, j] > 0:  # No need for epsilon here
+            # Momentum to velocity
+            grid_v[i, j] = (1 / grid_m[i, j]) * grid_v[i, j]
+            grid_v[i, j] += dt * gravity[None] * 30  # gravity
+            dist = attractor_pos[None] - dx * ti.Vector([i, j])
+            grid_v[i, j] += dist / (0.01 + dist.norm()) * attractor_strength[None] * dt * 100
+            if i < 3 and grid_v[i, j][0] < 0:
+                grid_v[i, j][0] = 0  # Boundary conditions
+            if i > n_grid - 3 and grid_v[i, j][0] > 0:
+                grid_v[i, j][0] = 0
+            if j < 3 and grid_v[i, j][1] < 0:
+                grid_v[i, j][1] = 0
+            if j > n_grid - 3 and grid_v[i, j][1] > 0:
+                grid_v[i, j][1] = 0
+    for p in x:  # grid to particle (G2P)
+        base = (x[p] * inv_dx - 0.5).cast(int)
+        fx = x[p] * inv_dx - base.cast(float)
+        w = [0.5 * (1.5 - fx) ** 2, 0.75 - (fx - 1.0) ** 2, 0.5 * (fx - 0.5) ** 2]
+        new_v = ti.Vector.zero(float, 2)
+        new_C = ti.Matrix.zero(float, 2, 2)
+        for i, j in ti.static(ti.ndrange(3, 3)):
+            # loop over 3x3 grid node neighborhood
+            dpos = ti.Vector([i, j]).cast(float) - fx
+            g_v = grid_v[base + ti.Vector([i, j])]
+            weight = w[i][0] * w[j][1]
+            new_v += weight * g_v
+            new_C += 4 * inv_dx * weight * g_v.outer_product(dpos)
+        v[p], C[p] = new_v, new_C
+        x[p] += dt * v[p]  # advection
 
 
 @ti.kernel
-def set_all_unused():
-    for p in F_used:
-        F_used[p] = 0
-        # basically throw them away so they aren't rendered
-        F_x[p] = ti.Vector([533799.0, 533799.0, 533799.0])
-        F_Jp[p] = 1
-        F_dg[p] = ti.Matrix([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
-        F_C[p] = ti.Matrix([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
-        F_v[p] = ti.Vector([0.0, 0.0, 0.0])
-
-
-def init_vols(vols):
-    set_all_unused()
-    total_vol = 0
-    for v in vols:
-        total_vol += v.volume
-
-    next_p = 0
-    for i, v in enumerate(vols):
-        v = vols[i]
-        if isinstance(v, CubeVolume):
-            par_count = int(v.volume / total_vol * n_particles)
-            if i == len(vols) - 1:  # this is the last volume, so use all remaining particles
-                par_count = n_particles - next_p
-            init_cube_vol(next_p, next_p + par_count, *v.minimum, *v.size, v.material)
-            next_p += par_count
-        else:
-            raise Exception("???")
-
-
-@ti.kernel
-def set_color_by_material(mat_color: ti.types.ndarray()):
+def reset():
+    group_size = n_particles // 3
     for i in range(n_particles):
-        mat = F_materials[i]
-        F_colors[i] = ti.Vector([mat_color[mat, 0], mat_color[mat, 1], mat_color[mat, 2], 1.0])
+        x[i] = [
+            ti.random() * 0.2 + 0.3 + 0.10 * (i // group_size),
+            ti.random() * 0.2 + 0.05 + 0.32 * (i // group_size),
+        ]
+        material[i] = 2  # 0: fluid 1: jelly 2: snow
+        v[i] = [0, 0]
+        F[i] = ti.Matrix([[1, 0], [0, 1]])
+        Jp[i] = 1
+        C[i] = ti.Matrix.zero(float, 2, 2)
 
 
-print("Loading presets...this might take a minute")
+print("[Hint] Use WSAD/arrow keys to control gravity. Use left/right mouse buttons to attract/repel. Press R to reset.")
+gui = ti.GUI("Taichi MLS-MPM-128", res=512, background_color=0x112F41)
+reset()
+gravity[None] = [0, -1]
 
-presets = [
-    [
-        CubeVolume(ti.Vector([0.55, 0.05, 0.55]), ti.Vector([0.4, 0.4, 0.4]), WATER),
-    ],
-    [
-        CubeVolume(ti.Vector([0.05, 0.05, 0.05]), ti.Vector([0.3, 0.4, 0.3]), WATER),
-        CubeVolume(ti.Vector([0.65, 0.05, 0.65]), ti.Vector([0.3, 0.4, 0.3]), WATER),
-    ],
-    [
-        # CubeVolume(ti.Vector([0.6, 0.05, 0.6]), ti.Vector([0.25, 0.25, 0.25]), SNOW),
-        CubeVolume(ti.Vector([0.35, 0.35, 0.35]), ti.Vector([0.25, 0.25, 0.25]), SNOW),
-        # CubeVolume(ti.Vector([0.05, 0.6, 0.05]), ti.Vector([0.25, 0.25, 0.25]), SNOW),
-    ],
-]
-preset_names = [
-    "Single Dam Break",
-    "Double Dam Break",
-    "Water Snow Jelly",
-]
+for frame in range(20000):
+    if gui.get_event(ti.GUI.PRESS):
+        if gui.event.key == "r":
+            reset()
+        elif gui.event.key in [ti.GUI.ESCAPE, ti.GUI.EXIT]:
+            break
+    if gui.event is not None:
+        gravity[None] = [0, 0]  # if had any event
+    if gui.is_pressed(ti.GUI.LEFT, "a"):
+        gravity[None][0] = -1
+    if gui.is_pressed(ti.GUI.RIGHT, "d"):
+        gravity[None][0] = 1
+    if gui.is_pressed(ti.GUI.UP, "w"):
+        gravity[None][1] = 1
+    if gui.is_pressed(ti.GUI.DOWN, "s"):
+        gravity[None][1] = -1
+    mouse = gui.get_cursor_pos()
+    gui.circle((mouse[0], mouse[1]), color=0x336699, radius=15)
+    attractor_pos[None] = [mouse[0], mouse[1]]
+    attractor_strength[None] = 0
+    if gui.is_pressed(ti.GUI.LMB):
+        attractor_strength[None] = 1
+    if gui.is_pressed(ti.GUI.RMB):
+        attractor_strength[None] = -1
+    for s in range(int(2e-3 // dt)):
+        substep()
+    gui.circles(
+        x.to_numpy(),
+        radius=1.5,
+        palette=[0x068587, 0xED553B, 0xEEEEF0],
+        palette_indices=material,
+    )
 
-curr_preset_id = 0
-
-paused = False
-
-use_random_colors = False
-particles_radius = 0.02
-
-material_colors = [(0.1, 0.6, 0.9), (0.93, 0.33, 0.23), (1.0, 1.0, 1.0)]
-
-
-def init():
-    global paused
-    init_vols(presets[curr_preset_id])
-
-
-init()
-
-res = (1080, 720)
-window = ti.ui.Window("Real MPM 3D", res, vsync=True)
-
-canvas = window.get_canvas()
-gui = window.get_gui()
-scene = window.get_scene()
-camera = ti.ui.Camera()
-camera.position(0.5, 1.0, 1.95)
-camera.lookat(0.5, 0.3, 0.5)
-camera.fov(55)
-
-
-def show_options():
-    global use_random_colors
-    global paused
-    global particles_radius
-    global curr_preset_id
-
-    with gui.sub_window("Presets", 0.05, 0.1, 0.2, 0.15) as w:
-        old_preset = curr_preset_id
-        for i in range(len(presets)):
-            if w.checkbox(preset_names[i], curr_preset_id == i):
-                curr_preset_id = i
-        if curr_preset_id != old_preset:
-            init()
-            paused = True
-
-    with gui.sub_window("Gravity", 0.05, 0.3, 0.2, 0.1) as w:
-        GRAVITY[0] = w.slider_float("x", GRAVITY[0], -10, 10)
-        GRAVITY[1] = w.slider_float("y", GRAVITY[1], -10, 10)
-        GRAVITY[2] = w.slider_float("z", GRAVITY[2], -10, 10)
-
-    with gui.sub_window("Options", 0.05, 0.45, 0.2, 0.4) as w:
-        use_random_colors = w.checkbox("use_random_colors", use_random_colors)
-        if not use_random_colors:
-            material_colors[WATER] = w.color_edit_3("water color", material_colors[WATER])
-            material_colors[SNOW] = w.color_edit_3("snow color", material_colors[SNOW])
-            material_colors[JELLY] = w.color_edit_3("jelly color", material_colors[JELLY])
-            set_color_by_material(np.array(material_colors, dtype=np.float32))
-        particles_radius = w.slider_float("particles radius ", particles_radius, 0, 0.1)
-        if w.button("restart"):
-            init()
-        if paused:
-            if w.button("Continue"):
-                paused = False
-        else:
-            if w.button("Pause"):
-                paused = True
-
-
-def render():
-    camera.track_user_inputs(window, movement_speed=0.03, hold_key=ti.ui.RMB)
-    scene.set_camera(camera)
-
-    scene.ambient_light((0, 0, 0))
-
-    colors_used = F_colors_random if use_random_colors else F_colors
-    scene.particles(F_x, per_vertex_color=colors_used, radius=particles_radius)
-
-    scene.point_light(pos=(0.5, 1.5, 0.5), color=(0.5, 0.5, 0.5))
-    scene.point_light(pos=(0.5, 1.5, 1.5), color=(0.5, 0.5, 0.5))
-
-    canvas.scene(scene)
-
-
-def main():
-    frame_id = 0
-
-    while window.running:
-        # print("heyyy ",frame_id)
-        frame_id += 1
-        frame_id = frame_id % 256
-
-        if not paused:
-            for _ in range(steps):
-                substep(*GRAVITY)
-
-        render()
-        show_options()
-        window.show()
-
-
-if __name__ == "__main__":
-    main()
+    # Change to gui.show(f'{frame:06d}.png') to write images to disk
+    gui.show()
--- MPM/mpm_snow.py
+++ MPM/mpm_snow.py
@@ -4,7 +4,7 @@ import taichi.math as tm
 
 ti.init(arch=ti.gpu)
 
-visualization = 0
+visualization = 1
 n_particle = 2000
 nx = ny = nz = 32
 
@@ -12,10 +12,10 @@ frame = 60
 substep = 30
 dt = 1 / (frame * substep)
 
-theta_c = 2.5e-2
-theta_s = 7.5e-3
+theta_c = 0.025
+theta_s = 0.0075
 
-E_0 = 1.4e3
+E_0 = 1.4e4
 niu = 0.2
 miu_0, lambda_0 = E_0 / (2 * (1 + niu)), E_0 * niu / ((1 + niu) * (1 - 2 * niu))  # Lame parameters
 
@@ -29,6 +29,42 @@ F_P_particle = ti.Matrix.field(3, 3, float, shape = n_particle)
 grid_vel = ti.Vector.field(3, float, shape = (nx, ny, nz))
 grid_mass = ti.field(float, shape = (nx, ny, nz))
 
+
+@ti.func
+def N(x):
+    x = ti.abs(x)
+    res = 0.0
+    if x < 0.5 :
+        res = 0.75 - x * x
+    elif x < 1.5 :
+        res = 0.5 * (1.5 - x) * (1.5 - x)
+    else:
+        res = 0.0
+
+    return res
+
+
+@ti.func
+def InDomain(x):
+    in_x = x[0] >= 0.0 and x[0] < nx
+    in_y = x[0] >= 0.0 and x[0] < ny
+    in_z = x[0] >= 0.0 and x[0] < nz
+    return in_x and in_y and in_z
+
+
+@ti.func
+def AtBoundary(x:int, y:int, z:int):
+    res = False
+    if x == nx-1 or x == 0:
+        res = True
+    if y == ny-1 or y == 0:
+        res = True
+    if z == nz-1 or z == 0:
+        res = True
+
+    return res
+
+
 @ti.kernel
 def Initiate():
     for i in pos_particle:
@@ -36,16 +72,21 @@ def Initiate():
         dy = (i % 100) % 10
         dz = i // 100
 
-        base = ti.Vector([10, 10, 10]) + ti.Vector([0.25, 0.25, 0.25])
+        base = ti.Vector([10, 10, 15]) + ti.Vector([0.25, 0.25, 0.25])
         dpos = ti.Vector([ti.random(float) - 0.5, ti.random(float) - 0.5, ti.random(float) - 0.5]) * 0.5
         pos_particle[i] = base + 0.5 * ti.Vector([dx, dy, dz]) + dpos
         vel_particle[i] = ti.Vector([0.0, 0.0, 0.0])
+        F_E_particle[i] = ti.Matrix.identity(float, 3)
+        F_P_particle[i] = ti.Matrix.identity(float, 3)
+        C_particle[i] = ti.Matrix.zero(float, 3, 3)
 
 
 @ti.kernel
 def Particle2Grid():
-    for p in pos_particle:
+    grid_mass.fill(0.0)
+    grid_vel.fill(0.0)
 
+    for p in pos_particle:
         # update F_E, F_P
         F_E_particle[p] = (ti.Matrix.identity(float, 3) + dt * C_particle[p]) @ F_E_particle[p]
         U, sigma, V = ti.svd(F_E_particle[p])
@@ -58,17 +99,87 @@ def Particle2Grid():
         # compute stress
         J_P = ti.Matrix.determinant(F_P_particle[p])
         J_E = ti.Matrix.determinant(F_E_particle[p])
-        miu = miu_0 * tm.exp(10.0 * (1 - J_P))
-        la = lambda_0 * tm.exp(10.0 * (1 - J_P))
+        # miu = miu_0 * tm.exp(10.0 * (1 - J_P))
+        # la = lambda_0 * tm.exp(10.0 * (1 - J_P))
+        miu = miu_0
+        la = lambda_0
         term1 = 2.0 * miu * (F_E_particle[p] - U @ V.transpose()) @ F_E_particle[p].transpose()
         term2 = ti.Matrix.identity(float, 3) * la * J_E * (J_E - 1.0)
         stress = term1 + term2
+        affine = C_particle[p] - dt * 1.25 * 4.0 * stress
 
-        center = int(pos_particle[p] + ti.Vector([0.5, 0.5, 0.5]))
+        # p2g
+        center = int(pos_particle[p] + ti.Vector([0.5, 0.5, 0.5])) # index of center grid
         for i, j, k in ti.ndrange((-1, 2), (-1, 2), (-1, 2)):
-            grid_pos = center + ti.Vector([i, j, k])
-            dpos = grid_pos - pos_particle[p]
-            weight = N(dpos[0]) * N(dpos[1]) * N(dpos[2])
+            grid_pos = float(center + ti.Vector([i, j, k]))
+            if InDomain(grid_pos):
+                dpos = grid_pos - pos_particle[p]
+                weight = N(dpos[0]) * N(dpos[1]) * N(dpos[2])
+                grid_mass[center[0]+i, center[1]+j, center[2]+k] += weight
+                grid_vel[center[0]+i, center[1]+j, center[2]+k] += weight * (vel_particle[p] + affine @ dpos)
+        
+
+@ti.kernel
+def Boundary():
+    for x, y, z in grid_vel:
+        if grid_mass[x, y, z] > 0.0:
+            grid_vel[x, y, z] /= grid_mass[x, y, z]
+            grid_vel[x, y, z] += dt * ti.Vector([0, 0, -9.8])
+
+            if x < 3 and grid_vel[x, y, z][0] < 0:
+                grid_vel[x, y, z][0] *= -0.2
+            if x > nx - 3 and grid_vel[x, y, z][0] > 0:
+                grid_vel[x, y, z][0] *= -0.2
+
+            if y < 3 and grid_vel[x, y, z][1] < 0:
+                grid_vel[x, y, z][1] *= -0.2
+            if y > ny - 3 and grid_vel[x, y, z][1] > 0:
+                grid_vel[x, y, z][1] *= -0.2
+
+            if z < 3 and grid_vel[x, y, z][2] < 0:
+                grid_vel[x, y, z][2] *= -0.2
+            if z > nz - 3 and grid_vel[x, y, z][2] > 0:
+                grid_vel[x, y, z][2] *= -0.2
+
+
+@ti.kernel
+def Grid2Particle():
+    for p in pos_particle:
+        vel_particle[p] = ti.Vector.zero(float, 3)
+        C_particle[p] = ti.Matrix.zero(float, 3, 3)
+
+        center = int(pos_particle[p] + ti.Vector([0.5, 0.5, 0.5])) # index of center grid
+        for i, j, k in ti.ndrange((-1, 2), (-1, 2), (-1, 2)):
+            grid_pos = float(center) + ti.Vector([i, j, k])
+            if InDomain(grid_pos):
+                dpos = grid_pos - pos_particle[p]
+                weight = N(dpos[0]) * N(dpos[1]) * N(dpos[2])
+                gridv = grid_vel[center[0]+i, center[1]+j, center[2]+k]
+                vel_particle[p] += weight * gridv
+                C_particle[p] += 4 * weight * gridv.outer_product(dpos)
+        
+        pos_particle[p] += dt * vel_particle[p]
+
+        # eps = 0.05
+        # if pos_particle[p][0] < 0:
+        #     pos_particle[p][0] = 0.05
+        #     vel_particle[p][0] = abs(vel_particle[p][0])
+        # if pos_particle[p][1] < 0:
+        #     pos_particle[p][1] = 0.05
+        #     vel_particle[p][1] = abs(vel_particle[p][1])
+        # if pos_particle[p][2] < 0:
+        #     pos_particle[p][2] = 0.05
+        #     vel_particle[p][2] = abs(vel_particle[p][2])
+
+        # if pos_particle[p][0] > nx - 1:
+        #     pos_particle[p][0] = nx - 1 - eps
+        #     vel_particle[p][0] = -abs(vel_particle[p][0])
+        # if pos_particle[p][1] > ny - 1:
+        #     pos_particle[p][1] = ny - 1 - eps
+        #     vel_particle[p][1] = -abs(vel_particle[p][1])
+        # if pos_particle[p][2] > nz - 1:
+        #     pos_particle[p][2] = nz - 1 - eps
+        #     vel_particle[p][2] = -abs(vel_particle[p][2])
 
 
 def main():
@@ -108,8 +219,8 @@ def main():
             cur_frame += 1
 
         print(cur_frame)
-        if cur_frame == 1800 :
-            exit()
+        # if cur_frame == 1800 :
+        #     exit()
 
 
 if __name__ == "__main__":
